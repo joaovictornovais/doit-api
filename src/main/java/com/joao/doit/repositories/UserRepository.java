f1674d75f9a3c83a371cf0c386c0c5c403e28c35
João Victor Novais <joao.dipaay@gmail.com>
2024-07-23 10:30:54 -0300

    feat: creating user repository

---
+++ src/main/java/com/joao/doit/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.joao.doit.repositories;
+
+import com.joao.doit.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
